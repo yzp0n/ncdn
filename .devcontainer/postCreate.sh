b1464dfbc184922745a0198622ec129c07c489d8
yzp0n <yzp0n@coe.ad.jp>
2024-07-23 14:56:58 +0000

    update devcontainer for xdp dev

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,9 +2,8 @@
 
 sudo apt update
 sudo apt install --no-install-recommends -y \
-    libssl-dev sshpass dnsutils \
-    supervisor iputils-ping tcpdump bind9-dnsutils
-
-# build-essential libbpf-dev clang llvm linux-headers-amd64
+    libssl-dev sshpass dnsutils ethtool \
+    supervisor iputils-ping tcpdump bind9-dnsutils \
+    build-essential libbpf-dev clang llvm linux-headers-$(uname -r)
 
 go install github.com/cespare/reflex@latest
