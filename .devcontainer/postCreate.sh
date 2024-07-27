69738105c27bf8228e540e5865c119bca3e7d594
yzp0n <yzp0n@coe.ad.jp>
2024-07-27 20:15:45 +0000

    split line for kernel headers install - since it may fail

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install --no-install-recommends -y \
     libssl-dev sshpass dnsutils ethtool \
     supervisor iputils-ping tcpdump bind9-dnsutils \
-    build-essential libbpf-dev clang llvm linux-headers-$(uname -r)
+    build-essential libbpf-dev clang llvm
+sudo apt install linux-headers-$(uname -r)
 
 go install github.com/cespare/reflex@latest
